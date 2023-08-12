6aec76ab0a28f08434764908ea3faa179e330066
Cyberufo44 <obelejunior10@gmail.com>
2023-08-12 10:19:34 -0700

    lowercase then uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
  * main - program that prints in lower, upper case then new line
@@ -7,12 +9,12 @@
  */
 int main(void)
 {
-int i;
+char letter;
 
-for (i = 'a'; i <= 'z'; i++)
-putchar(i);
-for (i = 'A'; i <= 'z'; i++)
-puchar(i);
+for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+for (letter = 'A'; letter <= 'z'; letter++)
+	puchar(letter);
 putchar('\n');
 return (0);
 }
