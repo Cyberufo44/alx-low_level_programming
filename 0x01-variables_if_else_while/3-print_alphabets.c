6534ae63c06fbcf6ae2da667780b17c9362aa53e
Cyberufo44 <obelejunior10@gmail.com>
2023-08-13 04:30:34 -0700

    corrections for 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,7 +14,7 @@ char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 	putchar(letter);
 for (letter = 'A'; letter <= 'z'; letter++)
-	puchar(letter);
+	putchar(letter);
 putchar('\n');
 return (0);
 }
